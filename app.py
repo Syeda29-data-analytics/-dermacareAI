6ab685bc5159d46078dbbb37d0012b7eb7ae635e
Syeda Umme Hani <syedah2910@gmail.com>
2026-06-03 08:36:54 +0530

    fix render deployment issues

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ def quiet_stderr():
 with quiet_stderr():
     from tensorflow.keras.models import load_model
 
+model = load_model("model.h5")
+
 BASE_DIR = Path(__file__).resolve().parent
 PROJECT_DIR = BASE_DIR.parent
 UPLOAD_FOLDER = PROJECT_DIR / "uploads"
@@ -391,4 +393,4 @@ def uploaded_file(filename: str):
 if __name__ == "__main__":
     logging.getLogger("werkzeug").disabled = True
     print("DermaCareAI website: http://127.0.0.1:5000", flush=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True) edit so i can copy and paste
